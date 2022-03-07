d09904dcb306e50f032f527b5af9ad0bead30bdb
Przemysław Dominikowski <przemyslaw.dominikowski@intel.com>
2022-03-07 09:23:33 +0100

    TDD workshop project
    Add TDD workshop project from previous labs

---
+++ StringCalculatorTests/UnitTest1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Xunit;
+
+namespace StringCalculatorTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        int res = TDD_IO2.StringCalculator.Calculate("");
+        Assert.Equal(0, res);
+    }
+
+    [Theory]
+    [InlineData("5", 5)]
+    [InlineData("25", 25)]
+    [InlineData("0", 0)]
+    [InlineData("300", 300)]
+    public void SingleNumberReturnsValue(string s, int expected)
+    {
+        int res = TDD_IO2.StringCalculator.Calculate(s);
+        Assert.Equal(res, expected);
+    }
+
+    [Theory]
+    [InlineData("5, 10, 20", 35)]
+    public void CommaSeparatedNumbersReturnSum(string s, int expected)
+    {
+        int res = TDD_IO2.StringCalculator.Calculate(s);
+        Assert.Equal(res, expected);
+    }
+
+    [Theory]
+    [InlineData("5\n10", 15)]
+    public void NewLineSeparatedNumbersReturnSum(string s, int expected)
+    {
+        int res = TDD_IO2.StringCalculator.Calculate(s);
+        Assert.Equal(res, expected);
+    }
+
+    [Theory]
+    [InlineData("5\n10, 30", 45)]
+    public void ThreeSeparatedNumbersReturnSum(string s, int expected)
+    {
+        int res = TDD_IO2.StringCalculator.Calculate(s);
+        Assert.Equal(res, expected);
+    }
+
+    [Theory]
+    [InlineData("-5")]
+    [InlineData("-7,12")]
+    [InlineData("12\n-7")]
+    public void NegativeNumbersThrowAnException(string s)
+    {
+        _ = Assert.Throws<ArgumentException>(() => TDD_IO2.StringCalculator.Calculate(s));
+    }
+
+    [Theory]
+    [InlineData("2048", 0)]
+    [InlineData("1024, 40", 40)]
+    [InlineData("10\n1000", 1010)]
+    public void NumbersGreaterThan1000ShouldBeIgnored(string s, int expected)
+    {
+        int res = TDD_IO2.StringCalculator.Calculate(s);
+        Assert.Equal(res, expected);
+    }
+
+    [Theory]
+    [InlineData("//#\n1000#30", 1030)]
+    [InlineData("//a\n1,2a3", 6)]
+    [InlineData("//1\n21314", 9)]
+    public void ExtraSeparatorIsSupported(string s, int expected)
+    {
+        int res = TDD_IO2.StringCalculator.Calculate(s);
+        Assert.Equal(res, expected);
+    }
+
+    [Theory]
+    [InlineData("//[Tymoteusz]\n1000Tymoteusz30", 1030)]
+    [InlineData("//[#]\n1,2#3", 6)]
+    [InlineData("//[13]\n21314", 16)]
+    public void MultiCharSeparatorIsSupported(string s, int expected)
+    {
+        int res = TDD_IO2.StringCalculator.Calculate(s);
+        Assert.Equal(res, expected);
+    }
+}
---
+++ TDD_IO2/StringCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TDD_IO2
+{
+	public class StringCalculator
+	{
+		private static readonly string[] delimeters = { ",", "\n" };
+
+		public static int Calculate(String s)
+		{
+			if (String.IsNullOrWhiteSpace(s))
+				return 0;
+
+			string[] localDelimeters = delimeters;
+			if (s.Length >= 3 && s.Substring(0, 2) == "//")
+            {
+				string[] str = s.Split("\n", 2);
+				s = str[1];
+
+				string delimeter;
+				if (str[0][2] == '[' && str[0][str[0].Length-1] == ']')
+				{
+					delimeter = str[0].Substring(3, str[0].Length - 4);
+				}
+				else delimeter = str[0][2].ToString();
+				localDelimeters = localDelimeters.Append(delimeter).ToArray();
+            }
+
+			int[] numbers = s.Split(localDelimeters, StringSplitOptions.None).Select(str => Int32.Parse(str)).ToArray();
+
+			int result = 0;
+			foreach(int num in numbers)
+            {
+				if (num < 0) throw new ArgumentException("Negative argument");
+				if (num > 1000) continue;
+				result += num;
+            }
+
+			return result;
+		}
+	}
+}
+
